afc9235b17d4668f2490d6a76a6d9725352559f7
Reeed24 <ra24afj@herts.ac.uk>
2025-12-04 22:41:18 +0000

    missing values are analysed in the datset

--- Projectfile.R
+++ Projectfile.R
@@ -4,3 +4,5 @@ modern_renewable_energy_consumption <- readr::read_csv(
   "modern-renewable-energy-consumption.csv"
 )
 str(modern_renewable_energy_consumption)
+
+print(colSums(is.na(modern_renewable_energy_consumption)))
