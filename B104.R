e41221e08e9e323c0ee507f4134a81c7d2653fb5
SindujaSevella <ss24ahm@herts.ac.uk>
2025-12-08 21:00:57 +0000

    Update B104.R

--- B104.R
+++ B104.R
@@ -10,3 +10,7 @@ ds <- subset(
   Entity %in% c("Europe", "Asia Pacific") &
     !is.na(`Solar (terawatt-hours)`)
 )
+t_test_result <- t.test(`Solar (terawatt-hours)` ~ Entity, data = ds)
+
+
+print(t_test_result)
